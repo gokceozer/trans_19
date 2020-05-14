a510e9a7943c9ebf8cf7eb7b28ab5170481fc7c1
nwlouis <64062613+nwlouis@users.noreply.github.com>
2020-05-14 13:50:13 +0800

    Add files via upload

--- covidapp/views.py
+++ covidapp/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from covidapp.forms import PatientForm, QueryForm, LocationForm, PastLocationForm #LocationFormSet
-from covidapp.models import Patient, Location
+from covidapp.models import Patient, Location, LocationTemplate
 from django.views.generic import DetailView, ListView, FormView, UpdateView, DeleteView
 from . import forms
 from django.views.generic.edit import FormMixin
@@ -8,8 +8,17 @@ from django.http import HttpResponseRedirect
 import datetime
 from collections import OrderedDict
 from django.urls import reverse_lazy, reverse
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User, Group
+from django import template
 
 # Create your views here.
+def userlogin(request):
+    form = AuthenticationForm(data=request.POST or None)
+    if form.is_valid():
+        return redirect('index')
+    return render(request, 'covidapp/login.html', {'form':form})
+
 def index(request):
     patient_dict = Patient.objects.order_by('case_number')
     my_dict = {'patients':patient_dict}
@@ -41,7 +50,6 @@ def location_new(request):
             print('ERROR FORM INVALID')
 
     return render(request, 'covidapp/location_new.html',{'locationForm':locationForm})
-
 
 class PatientDetailView(DetailView, FormMixin):
     model=Patient
@@ -94,14 +102,37 @@ class MyFormView(FormView):
 
     def form_valid(self, form): 
         print(form.cleaned_data) 
-        return super().form_valid(form) 
+        return super().form_valid(form)
+    
+class QueryView(FormView):
+    form_class = QueryForm
+    template_name = 'query_page.html'
+    
+
+    def form_valid(self, form): 
+        print(form.cleaned_data) 
+        return super().form_valid(form)
+
 
 def profile_search(request,pk):
-    if request.method == 'POST': 
-        query_form = QueryForm(request.POST)
+    items = Location.objects.filter(patient=pk)
+    if request.method == 'POST':
+        query_form = QueryForm(request.POST, items=items)
         if query_form.is_valid():
             model=Location
+            
             location = query_form.cleaned_data['location']
+            location_str = str(location).split(' Date')[0]
+            
+            idx1 = str(location).find('Date From: ')
+            idx2 = str(location).find(' Date To: ')
+            date_from = str(location)[idx1+11:idx2]
+            idx1 = str(location).find('Date To: ')
+            date_to = str(location)[idx1+9:]
+
+            date_from = datetime.datetime.strptime(date_from, '%Y-%m-%d')
+            date_to = datetime.datetime.strptime(date_to, '%Y-%m-%d')
+
             period = query_form.cleaned_data['period']
             
             
@@ -111,42 +142,114 @@ def profile_search(request,pk):
 
             #print(len(entry_list))
             for i in range(len(entry_list)):
+                
                 print(f"i is {entry_list[i].date_from}")
                 print(f"i is {entry_list[i].date_to}")
                 print(f"i is {entry_list[i].location_name}")
-                '''for j in range(i, len(entry_list)):
-                    print(type(entry_list[j].date_to))
-                    print(entry_list[i].date_to)
-                    print(entry_list[i].patient.name)
-                    if not (entry_list[i].date_from > entry_list[j].date_to + datetime.timedelta(days=period) \
-                            or entry_list[i].date_to < entry_list[j].date_from - datetime.timedelta(days=period)) \
-                            and entry_list[i].patient.idn != entry_list[j].patient.idn \
-                            and entry_list[i].location_name == entry_list[j].location_name:
+                if location_str == entry_list[i].location_name and location.patient.idn != entry_list[i].patient.idn \
+                    and not (date_from.date() > entry_list[i].date_to + datetime.timedelta(days=period) \
+                    or date_to.date() < entry_list[i].date_from - datetime.timedelta(days=period)):
+
                         
-                        if no_result:
-                            return_dict[counter] = entry_list[i]
+                    return_dict[i] = entry_list[i]
+                        
                             
-                            return_dict[counter] = entry_list[j]
-                            
-                        else:
-                            return_dict[counter] = entry_list[j]'''
-                            
-        
-            '''if return_dict[next(reversed(return_dict))] == 'done':
-                return_dict.popitem()'''
+
 
             #print(return_dict)
             return render(request, 'covidapp/query_page.html',{'return_dict':return_dict, 'query_form':query_form})
-                
     else:
-        query_form = QueryForm()
-
+        
+        query_form = QueryForm(items=items)
     return render(request, 'covidapp/query_page.html',{'query_form':query_form})
+
+
 
 class PatientUpdateView(UpdateView):
     form_class = PatientForm
     model = Patient
 
 class PatientDeleteView(DeleteView):
+
     model = Patient
     success_url = reverse_lazy('index')
+
+class LocationView(ListView):
+    model = LocationTemplate
+
+def location_temps(request):
+    locations = LocationTemplate.objects.all()
+    my_dict = {'locations':locations}
+    return render(request, 'covidapp/location_list.html',context=my_dict)
+
+class LocationDetailView(DetailView):
+    model = LocationTemplate
+    template_name = 'covidapp/location_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        #context['now'] = timezone.now()
+        return context
+
+    def get_success_url(self):
+        return reverse('location_detail', kwargs={'pk': self.object.pk})
+
+
+class LocationUpdateView(UpdateView):
+    #form_class = LocationForm
+    model = LocationTemplate
+    fields = ['address','district','grid_x','grid_y']
+    #template_name = 'locationTemplate_update_form'
+
+    def get_success_url(self):
+        return reverse('location_detail', kwargs={'pk': self.object.pk})
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            
+            for loc in Location.objects.all():
+                if self.object.location_name == loc.location_name:
+                    
+                    setattr(loc,'address',(form.cleaned_data['address']))
+                    setattr(loc,'district',(form.cleaned_data['district']))
+                    setattr(loc,'grid_x',(form.cleaned_data['grid_x']))
+                    setattr(loc,'grid_y',(form.cleaned_data['grid_y']))
+                    
+                    loc.save()
+
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    
+
+
+class LocationDeleteView(DeleteView):
+
+    model = LocationTemplate
+    success_url = reverse_lazy('location_list')
+
+    def post(self, request, *args, **kwargs):
+
+        self.object = self.get_object()
+        
+        for loc in Location.objects.all():
+            if self.object.location_name == loc.location_name:
+                loc.delete()
+        
+
+
+        return self.delete(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        instance = form.save(commit=False)
+
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super(LocationDeleteView, self).get_context_data(**kwargs)
+        
+        
+        return context
