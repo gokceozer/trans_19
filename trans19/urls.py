eb0f4ad4a0658d277e9ac3a94cfd6caad1dabe78
nwlouis <64062613+nwlouis@users.noreply.github.com>
2020-05-14 13:49:32 +0800

    Add files via upload

--- trans19/urls.py
+++ trans19/urls.py
@@ -13,13 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.contrib import admin, auth
 from django.urls import path, include
-from covidapp import views
+from django.contrib.auth import views
+from django.conf.urls import url
 
 urlpatterns = [
-    path('',views.index, name='index'),
     path('admin/', admin.site.urls),
     path('covidapp/', include('covidapp.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('password_change/', views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password_reset/', views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
 ]
