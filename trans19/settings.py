1a1ca59706714cb4016c9496db0b9177f5599ca1
nwlouis <64062613+nwlouis@users.noreply.github.com>
2020-05-16 17:40:08 +0800

    Add files via upload

--- trans19/settings.py
+++ trans19/settings.py
@@ -68,6 +68,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries':{
+                'has_group': 'covidapp.templatetags.has_group',
+            }
         },
     },
 ]
